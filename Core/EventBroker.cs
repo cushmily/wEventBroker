10c9a91ac2f021acb0f3b59b26884240cfed39f2
Skyrim Wu <cushmily@gmail.com>
2018-06-19 01:20:42 +0800

    - init proj

---
+++ Core/EventBroker.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wLib
+{
+    /// <summary>
+    /// IEventBroker implementation.
+    /// </summary>
+    public sealed class EventBroker : IEventBroker
+    {
+        private const int MaxCallDepth = 5;
+
+        private readonly Dictionary<Type, Delegate> _events = new Dictionary<Type, Delegate>(32);
+        private readonly Dictionary<Type, object> _cachedEvents = new Dictionary<Type, object>();
+
+        private int _eventsInCall;
+
+        public void Subscribe<T>(EvenetAction<T> eventAction)
+        {
+            if (eventAction != null)
+            {
+                var eventType = typeof(T);
+                Delegate rawList;
+                _events.TryGetValue(eventType, out rawList);
+                _events[eventType] = (rawList as EvenetAction<T>) + eventAction;
+            }
+        }
+
+        public void Unsubscribe<T>(EvenetAction<T> eventAction, bool keepEvent = false)
+        {
+            if (eventAction != null)
+            {
+                var eventType = typeof(T);
+                Delegate rawList;
+                if (_events.TryGetValue(eventType, out rawList))
+                {
+                    if (rawList != null)
+                    {
+                        var list = (EvenetAction<T>) rawList - eventAction;
+                        if (list == null && !keepEvent) { _events.Remove(eventType); }
+                        else { _events[eventType] = list; }
+                    }
+                }
+            }
+        }
+
+        public void UnsubscribeAll<T>(bool keepEvent = false)
+        {
+            var eventType = typeof(T);
+            Delegate rawList;
+            if (_events.TryGetValue(eventType, out rawList))
+            {
+                if (keepEvent) { _events[eventType] = null; }
+                else { _events.Remove(eventType); }
+            }
+        }
+
+        public void UnsubscribeAndClearAllEvents()
+        {
+            _events.Clear();
+        }
+
+        public void Publish<T>(T eventMessage)
+        {
+            if (_eventsInCall >= MaxCallDepth)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Max call depth reached");
+#endif
+                return;
+            }
+
+            var eventType = typeof(T);
+            Delegate rawList;
+            _events.TryGetValue(eventType, out rawList);
+            var list = rawList as EvenetAction<T>;
+            if (list != null)
+            {
+                _eventsInCall++;
+                try { list(eventMessage); }
+                catch (Exception ex) { Debug.LogError(ex); }
+
+                _eventsInCall--;
+            }
+        }
+
+        public void Publish<T>() where T : new()
+        {
+            var type = typeof(T);
+            if (!_cachedEvents.ContainsKey(type)) { _cachedEvents.Add(type, new T()); }
+
+            var evt = (T) _cachedEvents[type];
+            Publish(evt);
+        }
+    }
+}
---
+++ Core/IEventBroker.cs
@@ -0,0 +1,46 @@
+﻿namespace wLib
+{
+    /// <summary>
+    /// Prototype for subscribers action.
+    /// </summary>
+    /// <param name="eventData">Event data.</param>
+    public delegate void EvenetAction<in T>(T eventData);
+
+    public interface IEventBroker
+    {
+        /// <summary>
+        /// Subscribe callback to be raised on specific event.
+        /// </summary>
+        /// <param name="eventAction">Callback.</param>
+        void Subscribe<T>(EvenetAction<T> eventAction);
+
+        /// <summary>
+        /// Unsubscribe callback.
+        /// </summary>
+        /// <param name="eventAction">Event action.</param>
+        /// <param name="keepEvent">GC optimization - clear only callback list and keep event for future use.</param>
+        void Unsubscribe<T>(EvenetAction<T> eventAction, bool keepEvent = false);
+
+        /// <summary>
+        /// Unsubscribe all callbacks from event.
+        /// </summary>
+        /// <param name="keepEvent">GC optimization - clear only callback list and keep event for future use.</param>
+        void UnsubscribeAll<T>(bool keepEvent = false);
+
+        /// <summary>
+        /// Unsubscribe all listeneres and clear all events.
+        /// </summary>
+        void UnsubscribeAndClearAllEvents();
+
+        /// <summary>
+        /// Publish event.
+        /// </summary>
+        /// <param name="eventMessage">Event message.</param>
+        void Publish<T>(T eventMessage);
+
+        /// <summary>
+        /// Publish event.
+        /// </summary>
+        void Publish<T>() where T : new();
+    }
+}
