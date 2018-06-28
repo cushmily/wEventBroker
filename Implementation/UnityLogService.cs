199af0cb53cdd033155d6196ef132c9cf26fe557
Skyrim Wu <cushmily@gmail.com>
2018-06-28 10:40:46 +0800

    - add log service & reform this repo to wMisc

---
+++ Implementation/UnityLogService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace wLib
+{
+    public class UnityLogService : ILogService
+    {
+        private bool _doLog;
+
+        public void SwitchLog(bool toggle)
+        {
+            _doLog = toggle;
+        }
+
+        public void Log(object log)
+        {
+            if (_doLog) { Debug.Log(log); }
+        }
+
+        public void LogError(object error)
+        {
+            if (_doLog) { Debug.LogError(error); }
+        }
+
+        public void LogWarning(object warning)
+        {
+            if (_doLog) { Debug.LogWarning(warning); }
+        }
+
+        public void LogException(Exception exception)
+        {
+            if (_doLog) { Debug.LogException(exception); }
+        }
+    }
+}
---
+++ Interfaces/ILogService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace wLib
+{
+    public interface ILogService
+    {
+        void SwitchLog(bool toggle);
+
+        void Log(object log);
+
+        void LogError(object error);
+
+        void LogWarning(object warning);
+
+        void LogException(Exception exception);
+    }
+}
